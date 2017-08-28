1a58c591548ae9e062227b047c9d74e80c29ccf5
Justin Mao <justin.mao@mail.utoronto.ca>
2017-08-28 16:58:17 +0800

    Added demo page

---
+++ web/index.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',function(){
+  pss.setPageInit('p1', function() {
+    document.getElementById('bg').style.backgroundColor = '#AB82FF';
+  });
+  pss.setPageInit('p2', function() {
+    document.getElementById('bg').style.backgroundColor = '#FF926B';
+  });
+  pss.setPageInit('p3', function() {
+    document.getElementById('bg').style.backgroundColor = '#F4E26E';
+  });
+  pss.setPageInit('p4', function() {
+    document.getElementById('bg').style.backgroundColor = '#CDFF77';
+  });
+  pss.setPageInit('p5', function() {
+    document.getElementById('bg').style.backgroundColor = '#35D3FF';
+  });
+}, false);
